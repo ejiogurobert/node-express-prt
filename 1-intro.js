0c4c1f83193257ab7e9a4d15bb6c8f2a95c00d6f
Robert Ejiogu <robertejiogu@gmail.com>
2022-05-15 21:33:22 +0100

    introduction

---
+++ 1-intro.js
@@ -0,0 +1,16 @@
+const amount = 20;
+if (amount < 10) {
+  console.log("This is a small number");
+} else {
+  console.log("This is a large number");
+}
+
+console.log(`Robert first node code`);
+// console.log(ProcessingInstruction);
+// setTimeout(() => {
+//   console.log('this is the way');
+// }, 1000);
+setInterval(() => {
+  console.log('hello world');
+}, 1000);
+console.log(__dirname);
---
+++ app.js
@@ -0,0 +1,9 @@
+const rob = 'rob';
+const john = 'john';
+
+const sayHi = (name) => {
+  console.log(`Hi my name is ${name}`);
+};
+sayHi('Sarah')
+sayHi(rob);
+sayHi(john);
